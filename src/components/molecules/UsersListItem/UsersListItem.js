091523a36cc74f9de2a07ecd5a15b0cefeae4382
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-05-10 16:20:16 +0200

    added style files to the project

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import Average from 'components/atoms/Average/Average';
 import Button from 'components/atoms/Button/Button';
 import Label from 'components/atoms/Label/Label';
-
-const Wrapper = styled.li`
-  display: flex;
-  align-items: center;
-  position: relative;
-  &:not(:last-child)::after {
-    content: '';
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 1px;
-    background-color: lightgrey;
-  }
-`;
+import { Wrapper } from './UsersListItem.styles';
 
 const UsersListItem = ({ userData: { average, name, attendance } }) => (
   <Wrapper>
--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import { users } from 'data/users';
-import styled from 'styled-components';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
-
-const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.white};
-  width: 100%;
-  max-width: 500px;
-  padding: 40px 30px;
-  border-radius: 25px;
-  box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.3);
-`;
-
-const StyledList = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-`;
+import { Wrapper, StyledList } from './UsersList.styles.js';
 
 const UsersList = () => (
   <Wrapper>
