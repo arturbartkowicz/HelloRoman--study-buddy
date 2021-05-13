0abe396d59ce2abd2de9b64a4ea8b9e4e2b8da43
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-05-13 10:26:28 +0200

    initial commit

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -3,14 +3,23 @@ import { users } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper, StyledList } from './UsersList.styles.js';
 
-const UsersList = () => (
-  <Wrapper>
-    <StyledList>
-      {users.map((userData, i) => (
-        <UsersListItem index={i} userData={userData} />
-      ))}
-    </StyledList>
-  </Wrapper>
-);
+class UsersList extends React.Component {
+  state = {
+    isUsersList: false,
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <h1>{this.state.isUsersList ? 'Users List' : 'Students List'}</h1>
+        <StyledList>
+          {users.map((userData, i) => (
+            <UsersListItem index={i} userData={userData} />
+          ))}
+        </StyledList>
+      </Wrapper>
+    );
+  }
+}
 
 export default UsersList;
