1a1ec1b42eddb1c60235a54a4d9883aa490b63e4
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-05-20 16:51:03 +0200

    added use effect and use state to application

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,29 +1,83 @@
-import React from 'react';
-import { users } from 'data/users';
+import React, { useState, useEffect } from 'react';
+import { users as userData } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper, StyledList } from './UsersList.styles.js';
 
-class UsersList extends React.Component {
-  state = {
-    users,
+const mockAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (userData) {
+        resolve([...userData]);
+      } else {
+        reject({ message: 'Error' });
+      }
+    }, 2000);
+  });
+};
+
+// Komponent Funkcyjny
+
+const UsersList = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    mockAPI().then((data) => {
+      setUsers(data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  const deleteUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
   };
 
-  deleteUsers = (name) => {
-    const filteredName = this.state.users.filter((users) => users.name !== name);
-    this.setState({ users: filteredName });
-  };
+  return (
+    <Wrapper>
+      <h1>{isLoading ? 'Loading...' : 'Users list'}</h1>
+      <StyledList>
+        {users.map((userData, i) => (
+          <UsersListItem deleteUser={deleteUser} index={i} userData={userData} />
+        ))}
+      </StyledList>
+    </Wrapper>
+  );
+};
 
-  render() {
-    return (
-      <Wrapper>
-        <StyledList>
-          {this.state.users.map((userData, i) => (
-            <UsersListItem deleteUser={this.deleteUsers} index={i} userData={userData} />
-          ))}
-        </StyledList>
-      </Wrapper>
-    );
-  }
-}
+// Komponent klasowy
+// class UsersList extends React.Component {
+//   state = {
+//     users: [],
+//     isLoading: false,
+//   };
+
+//   componentDidMount() {
+//     this.setState({ isLoading: true });
+//     mockAPI().then( (data) => {
+//       this.setState({ users: data });
+//       this.setState({ isLoading: false });
+//     });
+//   }
+
+//   deleteUsers = (name) => {
+//     const filteredName = this.state.users.filter((users) => users.name !== name);
+//     this.setState({ users: filteredName });
+//   };
+
+//   render() {
+//     return (
+//       <Wrapper>
+//         <h1>{this.state.isLoading ? 'Loading...' : 'Users List'}</h1>
+//         <StyledList>
+//           {this.state.users.map((userData, i) => (
+//             <UsersListItem deleteUser={this.deleteUsers} index={i} userData={userData} />
+//           ))}
+//         </StyledList>
+//       </Wrapper>
+//     );
+//   }
+// }
 
 export default UsersList;
