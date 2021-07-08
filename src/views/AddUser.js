59f2fd5db434f838479bc639a1f684d529b2f869
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2021-07-08 16:28:09 +0200

    added use reducer hook

--- src/views/AddUser.js
+++ src/views/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext, useReducer } from 'react';
 import FormField from 'components/molecules/FormField/FormField';
 import { Button } from 'components/atoms/Button/Button';
 import { ViewWrapper } from 'components/molecules/ViewWrapper/ViewWrapper';
@@ -9,33 +9,74 @@ const initialFormState = {
   name: '',
   attendance: '',
   average: '',
+  consent: false,
+  error: '',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT CHANGE':
+      return {
+        ...state,
+        [action.field]: action.value,
+      };
+    case 'CLEAR VALUES':
+      return initialFormState;
+    case 'CONSENT TOGGLE':
+      return {
+        ...state,
+        consent: !state.consent,
+      };
+    case 'THROW ERROR':
+      return {
+        ...state,
+        error: action.errorValue,
+      };
+    default:
+      return state;
+  }
 };
 
 const AddUser = () => {
-  const [formValues, setFormValues] = useState(initialFormState);
+  const [formValues, dispatch] = useReducer(reducer, initialFormState);
   const { handleAddUser } = useContext(UsersContext);
   // const context = useContext(UsersContext);  wyzej destrukturyzacja
 
   const handleInputChange = (e) => {
-    setFormValues({
-      ...formValues,
-      [e.target.name]: e.target.value,
+    dispatch({
+      type: 'INPUT CHANGE',
+      field: e.target.name,
+      value: e.target.value,
     });
   };
 
   const handleSubmitUser = (e) => {
     e.preventDefault();
-    handleAddUser(formValues);
-    setFormValues(initialFormState);
+    if (formValues.consent) {
+      handleAddUser(formValues);
+      dispatch({ type: 'CLEAR VALUES' });
+    } else {
+      dispatch({ type: 'THROW ERROR', errorValue: 'You need to thick check box' });
+    }
   };
 
   return (
     <ViewWrapper as="form" onSubmit={handleSubmitUser}>
-      <Title>Add new student</Title>
+      {console.log(formValues)}
+      <Title>Add Title</Title>
       <FormField label="Name" id="name" name="name" value={formValues.name} onChange={handleInputChange} />
       <FormField label="Attendance" id="attendance" name="attendance" value={formValues.attendance} onChange={handleInputChange} />
       <FormField label="Average" id="average" name="average" value={formValues.average} onChange={handleInputChange} />
+      <FormField
+        label="Consent"
+        id="consent"
+        name="consent"
+        type="checkbox"
+        value={formValues.consent}
+        onChange={() => dispatch({ type: 'CONSENT TOGGLE' })}
+      />
       <Button type="submit">Add</Button>
+      {formValues.error ? <p>{formValues.error}</p> : null}
     </ViewWrapper>
   );
 };
