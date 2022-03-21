5ee28ae87099c2ae33613041943e5a4adc749ad3
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2022-03-21 10:05:32 +0100

    passed test

--- src/components/molecules/FormField/FormField.test.js
+++ src/components/molecules/FormField/FormField.test.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import { screen, fireEvent } from '@testing-library/react';
+import React, { useState } from 'react';
+import { screen, fireEvent, render } from '@testing-library/react';
 import FormField from './FormField';
 import renderWithThemeProvider from 'helpers/renderWithTeamProvider';
 
-describe('Form Field', () => {
+const InputWithButton = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <>
+      <input value={inputValue} onChange={handleInputChange} name="Name" id="name" placeholder="Siemano" />
+      <button disabled={!inputValue}>Submit</button>
+    </>
+  );
+};
+
+describe('Input with value', () => {
   it('Renders the component', () => {
-    renderWithThemeProvider(<FormField label="name" name="name" id="name" />);
+    render(<InputWithButton />);
+    screen.getByText('Submit');
+  });
+
+  it('Properly handles value change', () => {
+    render(<InputWithButton />);
+    const input = screen.getByPlaceholderText('Siemano');
+    fireEvent.change(input, { target: { value: 'Roman' } });
+    expect(input).toHaveValue('Roman');
   });
 });
---
+++ src/components/templates/NewsSection/NewsSection.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import NewsSection from './NewsSection';
+
+describe('News Section', () => {
+  it('Render articles', () => {
+    // render(<NewsSection />);
+    console.log('HI');
+  });
+});
