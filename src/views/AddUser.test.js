4be0b78a436fa13cddc392824b244896181f0fe9
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2022-03-25 07:28:47 +0100

    added checkbox to the AddUser test

--- src/views/AddUser.test.js
+++ src/views/AddUser.test.js
@@ -15,6 +15,10 @@ describe('Input with value', () => {
     fireEvent.change(screen.getByTestId('Name'), { target: { value: 'Dariuszek' } });
     fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
     fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.4' } });
+    const checkbox = screen.getByTestId('Consent');
+    expect(checkbox.checked).toEqual(false);
+    fireEvent.click(checkbox);
+    expect(checkbox.checked).toEqual(true);
     fireEvent.click(screen.getByText('Add'));
     screen.getByText('Dariuszek');
   });
