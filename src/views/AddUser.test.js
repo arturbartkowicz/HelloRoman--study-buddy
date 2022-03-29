b1ee2ef3100db83ca2d3dbf0d6680e0c79de6986
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2022-03-29 07:07:23 +0200

    added a new test when checkbox is not clicked

--- src/views/AddUser.test.js
+++ src/views/AddUser.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '@testing-library/jest-dom';
 import AddUser from './AddUser';
 import Dashboard from './Dashboard';
 import { screen, fireEvent } from '@testing-library/react';
 import { renderWithProviders } from 'helpers/renderWithProviders';
 
 describe('Input with value', () => {
-  it('Renders the component', () => {
+  it('Adds a new user to the list', () => {
     renderWithProviders(
       <>
         <AddUser />
@@ -15,11 +16,23 @@ describe('Input with value', () => {
     fireEvent.change(screen.getByTestId('Name'), { target: { value: 'Dariuszek' } });
     fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
     fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.4' } });
-    const checkbox = screen.getByTestId('Consent');
-    expect(checkbox.checked).toEqual(false);
-    fireEvent.click(checkbox);
-    expect(checkbox.checked).toEqual(true);
+    fireEvent.click(screen.getByTestId('Consent'));
     fireEvent.click(screen.getByText('Add'));
     screen.getByText('Dariuszek');
+  });
+
+  it('Prevents adding new user if the consent is not checked', () => {
+    renderWithProviders(
+      <>
+        <AddUser />
+        <Dashboard />
+      </>
+    );
+    fireEvent.change(screen.getByTestId('Name'), { target: { value: 'Packer' } });
+    fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
+    fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.4' } });
+    fireEvent.click(screen.getByText('Add'));
+    const newUser = screen.queryByText('Packer');
+    expect(newUser).not.toBeInTheDocument();
   });
 });
