bd67099df2dcf06d3575456317ea765e79562cc9
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2022-04-09 09:15:54 +0200

    added mock service and data to it

--- src/mocks/handlers/index.js
+++ src/mocks/handlers/index.js
@@ -1,1 +1,8 @@
-export const handlers = [];
+import { rest } from 'msw';
+import { students } from '../data/students';
+
+export const handlers = [
+  rest.get('/students', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ students }));
+  }),
+];
